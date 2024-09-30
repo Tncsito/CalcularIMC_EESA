db423a4dca142eb37b0570696949fe644a3f6068
SkimoCHK <alexguillen265@gmail.com>
2024-09-30 14:08:12 -0700

    Agregar archivos de proyecto.

---
+++ CalcularIMC_EESA/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CalcularIMC_EESA
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var Altura = double.Parse(altura.Text);//Altura es la lógica, y altura.Text es lo que llama la vista
+            var Peso = double.Parse(peso.Text);
+            var IMC = Peso /(Altura*Altura);
+
+            imc.Text = IMC.ToString();
+        }
+    }
+}
